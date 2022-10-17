979f007aaa448c92d1c916cfa08623f2e5c32160
ESESena <elanasena@hotmail.com>
2022-10-17 14:22:42 -0400

    Simple Search

--- CourseWebApp/Controllers/SalesRecordsController.cs
+++ CourseWebApp/Controllers/SalesRecordsController.cs
@@ -7,155 +7,47 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CourseWebApp.Data;
 using CourseWebApp.Models;
+using CourseWebApp.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace CourseWebApp.Controllers
 {
     public class SalesRecordsController : Controller
     {
-        private readonly CourseWebAppContext _context;
+        private readonly SalesRecordService _salesRecordService;
 
-        public SalesRecordsController(CourseWebAppContext context)
+        public SalesRecordsController(SalesRecordService salesRecordService)
         {
-            _context = context;
+            _salesRecordService = salesRecordService;
         }
 
         // GET: SalesRecords
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-              return View(await _context.SalesRecord.ToListAsync());
+              return View();
         }
 
-        // GET: SalesRecords/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            if (id == null || _context.SalesRecord == null)
+            if (!minDate.HasValue)
             {
-                return NotFound();
+                minDate = new DateTime(2000, 1, 1);
             }
-
-            var salesRecord = await _context.SalesRecord
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (salesRecord == null)
+            if (!maxDate.HasValue)
             {
-                return NotFound();
+                maxDate = DateTime.Now;
             }
+            ViewData["maxDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
 
-            return View(salesRecord);
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
-        // GET: SalesRecords/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //public async Task<IActionResult> GroupingSearch()
+        //{
+        //    return View(await _salesRecordService.ToListAsync());
+        //}
 
-        // POST: SalesRecords/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Date,Amount,Status")] SalesRecord salesRecord)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(salesRecord);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(salesRecord);
-        }
-
-        // GET: SalesRecords/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.SalesRecord == null)
-            {
-                return NotFound();
-            }
-
-            var salesRecord = await _context.SalesRecord.FindAsync(id);
-            if (salesRecord == null)
-            {
-                return NotFound();
-            }
-            return View(salesRecord);
-        }
-
-        // POST: SalesRecords/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Amount,Status")] SalesRecord salesRecord)
-        {
-            if (id != salesRecord.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(salesRecord);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SalesRecordExists(salesRecord.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(salesRecord);
-        }
-
-        // GET: SalesRecords/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.SalesRecord == null)
-            {
-                return NotFound();
-            }
-
-            var salesRecord = await _context.SalesRecord
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (salesRecord == null)
-            {
-                return NotFound();
-            }
-
-            return View(salesRecord);
-        }
-
-        // POST: SalesRecords/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.SalesRecord == null)
-            {
-                return Problem("Entity set 'CourseWebAppContext.SalesRecord'  is null.");
-            }
-            var salesRecord = await _context.SalesRecord.FindAsync(id);
-            if (salesRecord != null)
-            {
-                _context.SalesRecord.Remove(salesRecord);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool SalesRecordExists(int id)
-        {
-          return _context.SalesRecord.Any(e => e.Id == id);
-        }
     }
 }
